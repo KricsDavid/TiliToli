29fd82f1c2b0b0f616fa4abb35236b27ce31f86b
KricsDavid <david@kricsfalusi.hu>
2024-04-10 08:13:39 +0200

    5

--- TiliToli/MainWindow.xaml.cs
+++ TiliToli/MainWindow.xaml.cs
@@ -92,35 +92,81 @@ namespace TiliToli
                 int clickedNumber;
                 if (int.TryParse(clickedBtn.Content.ToString(), out clickedNumber))
                 {
-                    foreach (UIElement element in gridMain.Children)
+                    int rowClicked = Grid.GetRow(clickedBtn);
+                    int colClicked = Grid.GetColumn(clickedBtn);
+
+                    // Felfele mozgatás
+                    if (rowClicked > 0 && IsButtonEmpty(rowClicked - 1, colClicked))
                     {
-                        if (element is Button btn && btn != clickedBtn)
-                        {
-                            int rowClicked = Grid.GetRow(clickedBtn);
-                            int colClicked = Grid.GetColumn(clickedBtn);
-                            int rowBtn = Grid.GetRow(btn);
-                            int colBtn = Grid.GetColumn(btn);
+                        SwapButtons(clickedBtn, rowClicked - 1, colClicked);
+                        return;
+                    }
 
-                            if ((rowBtn == rowClicked && Math.Abs(colBtn - colClicked) == 1) ||
-                                (colBtn == colClicked && Math.Abs(rowBtn - rowClicked) == 1))
-                            {
-                                string btnContent = btn.Content.ToString();
-                                if (string.IsNullOrEmpty(btnContent))
-                                {
-                                    Grid.SetRow(clickedBtn, rowBtn);
-                                    Grid.SetColumn(clickedBtn, colBtn);
-                                    Grid.SetRow(btn, rowClicked);
-                                    Grid.SetColumn(btn, colClicked);
+                    // Lefele mozgatás
+                    if (rowClicked < 2 && IsButtonEmpty(rowClicked + 1, colClicked))
+                    {
+                        SwapButtons(clickedBtn, rowClicked + 1, colClicked);
+                        return;
+                    }
 
-                                    string tempContent = clickedBtn.Content.ToString();
-                                    clickedBtn.Content = btn.Content;
-                                    btn.Content = tempContent;
+                    // Balra mozgatás
+                    if (colClicked > 0 && IsButtonEmpty(rowClicked, colClicked - 1))
+                    {
+                        SwapButtons(clickedBtn, rowClicked, colClicked - 1);
+                        return;
+                    }
 
-                                    CheckGameCompletion();
-                                    return;
-                                }
-                            }
-                        }
+                    // Jobbra mozgatás
+                    if (colClicked < 2 && IsButtonEmpty(rowClicked, colClicked + 1))
+                    {
+                        SwapButtons(clickedBtn, rowClicked, colClicked + 1);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private bool IsButtonEmpty(int row, int col)
+        {
+            foreach (UIElement element in gridMain.Children)
+            {
+                if (element is Button btn)
+                {
+                    int btnRow = Grid.GetRow(btn);
+                    int btnCol = Grid.GetColumn(btn);
+                    if (btnRow == row && btnCol == col && string.IsNullOrEmpty(btn.Content.ToString()))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void SwapButtons(Button btn1, int row2, int col2)
+        {
+            int row1 = Grid.GetRow(btn1);
+            int col1 = Grid.GetColumn(btn1);
+
+            foreach (UIElement element in gridMain.Children)
+            {
+                if (element is Button btn2)
+                {
+                    int btnRow = Grid.GetRow(btn2);
+                    int btnCol = Grid.GetColumn(btn2);
+                    if (btnRow == row2 && btnCol == col2)
+                    {
+                        Grid.SetRow(btn1, row2);
+                        Grid.SetColumn(btn1, col2);
+                        Grid.SetRow(btn2, row1);
+                        Grid.SetColumn(btn2, col1);
+
+                        string tempContent = btn1.Content.ToString();
+                        btn1.Content = btn2.Content;
+                        btn2.Content = tempContent;
+
+                        CheckGameCompletion();
+                        break;
                     }
                 }
             }
